41933670ea670b34055bc6bc46af06c51b1b102d
Mulla0811 <aarbaj.mulla@hiddenbrains.in>
2024-04-03 18:42:02 +0530

    latest chnages

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'https://hrms-system.000webhostapp.com/employee/';
-$config['base_url'] = 'http://localhost/HRMS/';
+$config['base_url'] = 'http://localhost/extra_work/HRMS/';
 $config['site_path'] = '';
 
 
---
+++ application/views/templates_c/7a1c09e16558bf5de3f1ad74fed98cbf44e9bdb5_0.file.department.tpl.php
@@ -0,0 +1,324 @@
+<?php
+/* Smarty version 4.3.2, created on 2024-04-03 18:34:02
+  from '/var/www/html/extra_work/HRMS/application/views/templates/department.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.2',
+  'unifunc' => 'content_660d53c22d1b31_16043084',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '7a1c09e16558bf5de3f1ad74fed98cbf44e9bdb5' => 
+    array (
+      0 => '/var/www/html/extra_work/HRMS/application/views/templates/department.tpl',
+      1 => 1711730821,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:footer.tpl' => 1,
+  ),
+),false)) {
+function content_660d53c22d1b31_16043084 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div class="main-middle-container">
+	 <aside class="right-sidebar">
+    <!-- Sidebar scroll-->
+    <div>
+        <div class="brand-logo d-flex align-items-center justify-content-between filter-title-bolock">
+            <a href="javascript:void(0)" class="text-nowrap logo-img">
+                Filter
+            </a>
+            <div class="close-filter-btn d-xl-none d-block filter-popup cursor-pointer" >
+                <i class="ti ti-x fs-8"></i>
+            </div>
+        </div>
+
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar filter-block" data-simplebar="init">
+            <div class="simplebar-wrapper" style="margin: 0px -24px;">
+                <div class="simplebar-height-auto-observer-wrapper"><div class="simplebar-height-auto-observer"></div></div>
+                <div class="simplebar-mask">
+                    <div class="simplebar-offset" style="right: -7px; bottom: 0px;">
+                        <div class="simplebar-content-wrapper" tabindex="0" role="region" aria-label="scrollable content" style="height: 100%; overflow-y: scroll;">
+                            <div class="simplebar-content" style="padding: 0px 15px;">
+                                <ul id="sidebarnav" class="in">
+                                    
+                                    <div class="filter-row">
+                                        <li class="nav-small-cap">
+                                            <span class="hide-menu">Department Name </span>
+
+                                            <span class="search-show-hide float-right"><i class="ti ti-minus"></i></span>
+                                        </li>
+                                        <li class="sidebar-item">
+                                            <div class="input-group">
+                                              <input type="text" id="department_name_search" class="form-control" placeholder="Department Name">
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div class="filter-row">
+                                        <li class="nav-small-cap">
+                                            <span class="hide-menu">Department Code</span>
+                                            <span class="search-show-hide float-right"><i class="ti ti-minus"></i></span>
+                                        </li>
+                                        <li class="sidebar-item">
+                                            <div class="input-group">
+                                              <input type="text" id="department_code_search" class="form-control" placeholder="Department Code">
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div class="filter-row">
+                                        <li class="nav-small-cap">
+                                            <span class="hide-menu">Added By</span>
+                                            <span class="search-show-hide float-right"><i class="ti ti-minus"></i></span>
+                                        </li>
+                                        <li class="sidebar-item">
+                                            <div class="input-group ">
+                                              <input type="text" class="form-control" id="added_by_search"  placeholder="Added By">
+                                              <!-- <span class="input-group-text"><i class="las la-calendar-alt"></i></span> -->
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div class="filter-row">
+                                        <li class="nav-small-cap">
+                                            <span class="hide-menu">Added Date</span>
+                                            <span class="search-show-hide float-right"><i class="ti ti-minus"></i></span>
+                                        </li>
+                                        <li class="sidebar-item">
+                                            <div class="input-group">
+                                              <input type="text" id="added_date_search" class="form-control"  placeholder="Added Date">
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div class="filter-row">
+                                        <li class="nav-small-cap">
+                                            <span class="hide-menu">Updated By</span>
+                                            <span class="search-show-hide float-right"><i class="ti ti-minus"></i></span>
+                                        </li>
+                                        <li class="sidebar-item">
+                                            <div class="input-group ">
+                                              <input type="text" class="form-control" id="updated_by_search"  placeholder="Updated By">
+                                              <!-- <span class="input-group-text"><i class="las la-calendar-alt"></i></span> -->
+                                            </div>
+                                        </li>
+                                    </div>
+                                    <div class="filter-row">
+                                        <li class="nav-small-cap">
+                                            <span class="hide-menu">Updated Date</span>
+                                            <span class="search-show-hide float-right"><i class="ti ti-minus"></i></span>
+                                        </li>
+                                        <li class="sidebar-item">
+                                            <div class="input-group ">
+                                              <input type="text" class="form-control" id="updated_date_search"  placeholder="Updated Date">
+                                              <!-- <span class="input-group-text"><i class="las la-calendar-alt"></i></span> -->
+                                            </div>
+                                        </li>
+                                    </div>
+                                </ul>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="simplebar-placeholder" style="width: auto; height: 810px;"></div>
+            </div>
+            <div class="simplebar-track simplebar-horizontal" style="visibility: hidden;"><div class="simplebar-scrollbar" style="width: 0px; display: none;"></div></div>
+            <div class="simplebar-track simplebar-vertical" style="visibility: visible;"><div class="simplebar-scrollbar" style="height: 214px; display: block; transform: translate3d(0px, 0px, 0px);"></div></div>
+        </nav>
+         <div class="filter-popup-btn">
+            <button class="btn btn-primary reset-filter">Reset</button>
+            <button class="btn btn-primary search-filter">Search</button>
+        </div>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
+	<div class="sub-header att-sub-header">
+		<div class="sub-header-left pull-left">
+			<h3>Department</h3>
+		</div>
+		<div class="sub-header-right pull-right">
+			<div class="timesheet-summary icon-box">
+				<div class="timesheet-summary-lst">
+                   <div class="dropdown grid-drop-down reset-filter" title="Reset Filter">
+                        <button class="btn btn-secondary top-btn-row" type="button"  >
+                            <i class="ti ti-refresh " style="color: black"></i> 
+                        </button>
+                    </div>
+                </div>
+            <div class="timesheet-summary-lst">
+                   <div class="dropdown grid-drop-down filter-popup" title="Filter">
+                        <button class="btn btn-secondary top-btn-row" type="button"  >
+                            <i class="ti ti-filter " style="color: black"></i> 
+                        </button>
+                    </div>
+                </div>
+				<div class="timesheet-summary-lst">
+					<button type="button" class="btn btn-primary add-department  add-action">
+						<i class="ti ti-plus"></i>
+						<span>Add Department</span>
+					</button>
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<div class="inner-container" style="position: static; zoom: 1;">
+		<div class="timesheet-container">
+
+			<div class="custom-modal-content custom-content-table-with-fixed-column ">
+				
+				<table width="100%" border="1" cellspacing="0" cellpadding="0" class="table leave-list-table display" style="border-collapse: collapse;" border-color="#e1e1e1">
+					<thead>
+						<tr class="text-center">
+							<!-- <th scope="col">#</th> -->
+							<th scope="col">Department Name</th>
+							<th scope="col">Department Code</th>
+							<th scope="col">Added By</th>
+							<th scope="col">Added Date</th>
+							<th scope="col">Updated By</th>
+							<th scope="col">Updated Date</th>
+							<th scope="col">Action</th>
+						</tr>
+					</thead>
+					<tbody id="leave_data_body" tabindex="5001" style="overflow: hidden; outline: none;">
+						<?php if (count($_smarty_tpl->tpl_vars['department']->value) > 0) {?>
+
+						<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['department']->value, 'department_row', false, 'department_index');
+$_smarty_tpl->tpl_vars['department_row']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['department_index']->value => $_smarty_tpl->tpl_vars['department_row']->value) {
+$_smarty_tpl->tpl_vars['department_row']->do_else = false;
+?>
+						<tr class="department-request-row">
+							<!-- <td><?php echo $_smarty_tpl->tpl_vars['designation_index']->value+1;?>
+</td> -->
+							<td class="departmen_name"><?php echo $_smarty_tpl->tpl_vars['department_row']->value['departmen_name'];?>
+</td>
+							<td class="department_code"><?php echo $_smarty_tpl->tpl_vars['department_row']->value['department_code'];?>
+</td>
+							<td><?php echo display_no_character($_smarty_tpl->tpl_vars['department_row']->value['added_by_name']);?>
+</td>
+							<td><?php echo date_formater($_smarty_tpl->tpl_vars['department_row']->value['added_date']);?>
+</td>
+							<td><?php echo display_no_character($_smarty_tpl->tpl_vars['department_row']->value['updated_by_name']);?>
+</td>
+							<td><?php echo date_formater($_smarty_tpl->tpl_vars['department_row']->value['updated_date']);?>
+</td>
+							<td><span class="edit_department me-2 text-secondary cursor" data-id="<?php echo $_smarty_tpl->tpl_vars['department_row']->value['department_id'];?>
+" data-company-id="<?php echo $_smarty_tpl->tpl_vars['department_row']->value['company_id'];?>
+" title="Edit"><i class=" la-edit ti ti-edit"></i></span></td>
+							
+
+					</tr>
+					<?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+					
+					<?php }?>
+				</tbody>
+			</table>
+
+			</div>
+		</div>
+	</div>
+
+</div>
+
+
+<div class="modal fade leave-popup-block" id="designation_popup" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+	<div class="modal-dialog modal-dialog-centered ">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalToggleLabel">Department</h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<form id="DepartmentForm" action="javascript:void(0)" method="post">
+				<div class="modal-body">
+					<div class="main-content">
+
+						<div class="card mb-3 leave-box">
+							<div class="row g-0 m-3">
+								<div class="mb-3">
+									<label for="designationName" class="form-label">Department Name</label>
+									<input type="text" class="form-control department_name" id="department_name" name="department_name"  placeholder="Enter Department Name">
+								</div>
+                                <div class="mb-3 select-box-block" <?php if ($_smarty_tpl->tpl_vars['user_data']->value['role'] != 'arom') {?> style="display: none" <?php }?>>
+                                    <label for="company_id" class="form-label">Company</label>
+                                    <select class="form-select company_id" name="company_id" id="company_id">
+                                        <option value="">Select Company</option>
+                                        <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['company_details']->value, 'company_name');
+$_smarty_tpl->tpl_vars['company_name']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['company_name']->value) {
+$_smarty_tpl->tpl_vars['company_name']->do_else = false;
+?>
+                                            <?php if ($_smarty_tpl->tpl_vars['company_name']->value['company_id'] == $_smarty_tpl->tpl_vars['selected_company']->value) {?>
+                                                <option value="<?php echo $_smarty_tpl->tpl_vars['company_name']->value['company_id'];?>
+" selected><?php echo $_smarty_tpl->tpl_vars['company_name']->value['company_name'];?>
+</option>
+                                            <?php } else { ?>
+                                                <option value="<?php echo $_smarty_tpl->tpl_vars['company_name']->value['company_id'];?>
+"><?php echo $_smarty_tpl->tpl_vars['company_name']->value['company_name'];?>
+</option>
+                                            <?php }?>
+                                        <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+                                    </select>
+                                </div>
+								<div class="mb-3">
+									<label for="designationName" class="form-label">Department Code</label>
+									<input type="text" class="form-control department_code" id="department_code" name="department_code"  placeholder="Enter Department Code">
+								</div>
+								<input type="hidden" class="form-control department_id"  name="department_id" id="department_id">
+								<input type="hidden" class="form-control mode"  name="mode" id="mode">
+							</div>
+						</div>
+					</div>
+
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-primary submit-holiday">Submit</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+</div>
+<!-- extra for footer -->
+</div>
+</div>
+</div>
+</div>
+<?php echo '<script'; ?>
+>
+    var no_data_message =  <?php ob_start();
+echo json_encode($_smarty_tpl->tpl_vars['no_data_message']->value);
+$_prefixVariable1 = ob_get_clean();
+echo $_prefixVariable1;?>
+;
+    var selected_company = <?php ob_start();
+echo json_encode($_smarty_tpl->tpl_vars['selected_company']->value);
+$_prefixVariable2 = ob_get_clean();
+echo $_prefixVariable2;?>
+
+<?php echo '</script'; ?>
+>
+
+<link rel="stylesheet" href="public/css/attendance_sheet.css" />
+<link rel="stylesheet" href="public/css/leave.css" />
+<link rel="stylesheet" href="public/css/department.css" />
+<?php echo '<script'; ?>
+ src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="public/js/department.js"><?php echo '</script'; ?>
+>
+<?php $_smarty_tpl->_subTemplateRender("file:footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}
+}
